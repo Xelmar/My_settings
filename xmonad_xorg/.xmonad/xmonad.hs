f513c77ea5915e402f5104ed86963bd76dc0bfd4
xelmar <xelmar8@gmail.com>
2017-07-17 11:19:11 +0300

    Update

--- xmonad_xorg/.xmonad/xmonad.hs
+++ xmonad_xorg/.xmonad/xmonad.hs
@@ -69,7 +69,7 @@ defaults = defaultConfig {
 	}`additionalKeys` myKeys
 
 myWorkspaces :: [String]
-myWorkspaces =  ["1:WorkGround","2:dev","3:web","4:vm","5:media"] ++ map show [6..9]
+myWorkspaces =  ["1:WorkGround","2:dev","3:web","4:apps","5:media"] ++ map show [6..9]
 
 -- tab theme default
 myTabConfig = defaultTheme {
@@ -84,7 +84,7 @@ myTabConfig = defaultTheme {
 xmobarTitleColor = "#FFFFFF" --"#FFB6B0"
 
 -- Color of current workspace in xmobar.
-xmobarCurrentWorkspaceColor = "green"
+xmobarCurrentWorkspaceColor = "#8AE234"
 
 myLayoutHook = spacing 6 $ gaps [(U,28),(D,6),(R,6),(L,6)] $ toggleLayouts (noBorders Full) $ 
     smartBorders $ Mirror tiled ||| mosaic 2 [3,2]  ||| tabbed shrinkText myTabConfig
@@ -108,23 +108,22 @@ myManageHook = composeAll . concat $
 	[ [className =? c --> doF (W.shift "1:WorkGround")		| c <- myWorkG]
 	, [className =? c --> doF (W.shift "2:dev")		| c <- myDev]
 	, [className =? c --> doF (W.shift "3:web")		| c <- myWeb]
-	, [className =? c --> doF (W.shift "4:vm")		| c <- myVMs]
+	, [className =? c --> doF (W.shift "4:apps")		| c <- myApps]
 	, [className =? c --> doF (W.shift "5:media")		| c <- myMedia]
 	, [manageDocks]
 	]
 	where
 	myWorkG = ["python"]
-	myDev = ["Eclipse","Gedit","sublime-text"]
-	myWeb = ["Firefox","Chromium","Google-chrome"]
-	--myTerm = ["Terminator","xterm"]
-        myVMs = ["VirtualBox"]
-	myMedia = ["Steam"]
+	myDev = ["RStudio"]
+	myWeb = ["Firefox","Chromium"]
+	myApps = ["VirtualBox","Thunar"]
+	myMedia = ["Steam","mocp"]
 	
 	--KP_Add KP_Subtract
 myKeys = [
            ((mod4Mask, xK_Right), nextScreen) 
          , ((mod4Mask .|. controlMask, xK_Left ), prevScreen)
-         , ((mod4Mask, xK_w), goToSelected defaultGSConfig)
+         , ((mod4Mask, xK_o), goToSelected defaultGSConfig)
 	 	     , ((mod4Mask, xK_s), spawnSelected defaultGSConfig ["chromium","idea","gvim"])
 	 	     , ((mod4Mask, xF86XK_AudioRaiseVolume), spawn "amixer -c 1 sset Master 5%+ && ~/.xmonad/getvolume.sh > /tmp/.volume-pipe")
 	 	     , ((mod4Mask, xF86XK_AudioLowerVolume), spawn "amixer -c 1 sset Master 5%- && ~/.xmonad/getvolume.sh > /tmp/.volume-pipe")
@@ -132,6 +131,10 @@ myKeys = [
          , ((mod4Mask, xF86XK_MonBrightnessDown), spawn "brightnessctl set 10%-")
          , ((mod4Mask, xF86XK_MonBrightnessUp), spawn "brightnessctl set 10%+")
          , ((mod4Mask, xK_p), spawn "dmenu_run -x 6 -y 8 -h 16 -w 1354")
+         , ((mod4Mask, xK_f), spawn "thunar")
+         , ((mod4Mask, xK_w), spawn "chromium")
+         , ((mod4Mask, xK_m), spawn "mocp")
+         , ((mod4Mask .|. shiftMask, xK_t), spawn "urxvt")
          ]
                    
 
@@ -141,7 +144,7 @@ main = do
 	xmonad $ defaults {
 	logHook =  dynamicLogWithPP $ defaultPP {
             ppOutput = System.IO.hPutStrLn xmproc
-          , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100 .wrap "  [ <fc=gray> " " </fc> ]  "
+          , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100 .wrap "  [ <fc=orange> " " </fc> ]  "
           , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor "" . wrap "[" "]"
           , ppSep = "   "
           , ppWsSep = " "
